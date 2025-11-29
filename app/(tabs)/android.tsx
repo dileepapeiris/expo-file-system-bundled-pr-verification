2bd8f497aa5f362782493a167bb48ac1637e1c4b
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-11-29 13:01:47 +0530

    Add initial android.tsx tab component
    Created a new android.tsx file under app/(tabs) with initial imports and file header. This sets up the structure for the Android-specific tab component.

---
+++ app/(tabs)/android.tsx
@@ -0,0 +1,20 @@
+/**
+ * ------------------------------------------------------------
+ *  Owner    : K. Dileepa Thushan Peiris
+ *  GitHub   : https://github.com/dileepapeiris
+ *  Created  : 2025-11-29
+ * ------------------------------------------------------------
+ */
+
+import { Asset } from "expo-asset";
+import { File, Paths } from "expo-file-system";
+import React, { useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+

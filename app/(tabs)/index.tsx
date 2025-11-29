4c3b098047c0daf206a4b1fff01137f55164f5b4
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-11-29 13:00:11 +0530

    Add index page to redirect to /ios tab
    Created a new index.tsx file under the (tabs) directory that redirects users to the /ios tab using expo-router's Redirect component.

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * ------------------------------------------------------------
+ *  Owner    : K. Dileepa Thushan Peiris
+ *  GitHub   : https://github.com/dileepapeiris
+ *  Created  : 2025-11-29
+ * ------------------------------------------------------------
+ */
+
+import { Redirect } from "expo-router";
+
+export default function Index() {
+  return <Redirect href="/(tabs)/ios" />;
+}

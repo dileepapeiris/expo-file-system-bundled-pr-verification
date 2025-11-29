90911bff27d1244c75b097b4dc6ff67bc73cd429
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-11-29 13:00:27 +0530

    Add initial tab layout component
    Introduces the TabLayout component using expo-router Tabs, custom haptic tab button, and color scheme support. Sets up screen options for tab bar appearance and disables the header.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,26 @@
+/**
+ * ------------------------------------------------------------
+ *  Owner    : K. Dileepa Thushan Peiris
+ *  GitHub   : https://github.com/dileepapeiris
+ *  Created  : 2025-11-29
+ * ------------------------------------------------------------
+ */
+
+import { Tabs } from "expo-router";
+import React from "react";
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}
+    >
